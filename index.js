d8b690497cca1a854af2a8f3df0945607f249d52
tejaswini15-github <pendotatejaswini2000@gmail.com>
2024-11-24 03:38:31 +0530

    string methods

---
+++ index.js
@@ -0,0 +1,148 @@
+str ="Javascript is Awesome";
+console.log(str.slice(4,-5)); 
+console.log(str.slice(0,-12)); 
+console.log(str.substring(4,-5)); 
+console.log(str.substring()); 
+console.log(str.substring(4,4)); 
+console.log(str.substring(4,5)); 
+console.log(str.substring(-6,5)); 
+console.log(str.substr());
+
+console.log(str.substr(4)); 
+console.log(str.substring(4)); 
+console.log(str.slice(4)); 
+console.log(str.substr(4,7)); 
+console.log(str.substr(4,-1)); 
+console.log(str.substr(-1,10)); 
+console.log(str.substr(-2,10)); 
+console.log(str.toUpperCase()); 
+console.log(str.toLowerCase()); 
+
+console.log(str.split()); 
+console.log(str.split("")); 
+console.log(str.split(" ")); 
+console.log(str.split("a")); 
+console.log(str.split("A")); 
+console.log(str.split("",5)); 
+console.log(str.split(" ",1)); 
+
+console.log(str.concat("esdrfgh")); 
+
+console.log(str.slice(0,1).toUpperCase()); 
+console.log(str.charAt(0).toUpperCase()); 
+
+console.log(str.at(0).toUpperCase()+str.slice(1,11)+str.at(11).toUpperCase()+str.slice(12,14)+str.at(14).toUpperCase()+str.slice(15)); 
+
+console.log(str.concat("a","b","c","d","e","f"));
+
+str='Hello world';
+console.log(str.replace('world','Universe'))
+
+str='foo bar baz';
+console.log(str.replaceAll('bar','baz'))
+
+str='Hello'
+console.log(str.charAt(4)); 
+
+str='Hello World'
+console.log(str.charAt(-1));
+
+str='Hello'
+console.log(str.at(-1));
+
+str='Hello World'
+console.log(str.length);
+
+str='Javascript'
+console.log(str.slice(4, 10))
+
+str='Javascript'
+console.log(str.slice(-6))
+
+str='Javascript'
+console.log(str.substring(4, 10))
+
+str='Hello'
+console.log(str.substring(-2))
+
+str='Hello'
+console.log(str.charCodeAt(1))
+
+str='Hello'
+console.log(str.toUpperCase())
+
+str='Hello World'
+console.log(str.split(" "))
+
+str='a,b,c'
+console.log(str.split(","))
+
+str='abcabcabc'
+console.log(str.split("a", 2))
+
+str='JavaScript is fun'
+console.log(str.indexOf("is"))
+
+str='JavaScript'
+console.log(str.slice(-4,-1))
+
+str='123456789'
+console.log(str.substr(3,4))
+
+str='JavaScript'
+console.log(str.startsWith('Java'))
+
+str='JavaScript'
+console.log(str.endsWith('Script'))
+
+str=' Hello '
+console.log(str.trim())
+
+str='abcDEF'
+console.log(str.toLowerCase())
+
+str='Hello'
+console.log(str[0])
+
+str='Hello'
+console.log(str.charAt(10))
+
+str='Hello World'
+console.log(str.includes('World'))
+
+str='Hello'
+console.log(str.repeat(3))
+
+str=' '
+console.log(str.trim().length)
+
+str='JavaScript'
+console.log(str.replace('Script', 'Fun'))
+
+str='Hello'
+console.log(str.padStart(10, '*'))
+
+str='Hello'
+console.log(str.padEnd(10, '!'))
+
+str='JavaScript is cool'
+console.log(str.lastIndexOf('is'))
+
+str='Test123'
+console.log(str.startsWith('T',1))
+
+str='Test123'
+console.log(str.endsWith('3',6))
+
+str='JavaScript'
+console.log(str.slice(4, -2))
+
+str='abcdef'
+console.log(str.split("", 3))
+
+str=' spaced out '
+console.log (str.trimStart ())
+
+str='12345'
+console.log(str.indexOf('6'))
+
